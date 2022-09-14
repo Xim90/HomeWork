d844dcd1f97dee30bd3e66ab75a7c0de86c4f829
Kameisha <xim9052@gmail.com>
2022-09-14 14:47:05 +0300

    fix pattern of expression

--- src/by/it_academy/Constants.java
+++ src/by/it_academy/Constants.java
@@ -1,10 +1,10 @@
 package by.it_academy;
 
 public class Constants {
-    public static final String PATTERN_NUMBERS = "-?\\d+(.\\d+)?";
+    public static final String PATTERN_NUMBERS = "-?\\d+(\\.\\d+)?";
     public static final String PATTERN_OPERATIONS = "(?<=[^(])[\\-+/*]";
     public static final String PATTERN_EXPRESSION =
-            PATTERN_NUMBERS + PATTERN_OPERATIONS + "(\\()?" + PATTERN_NUMBERS + "(\\))?";
+            "^"+PATTERN_NUMBERS + PATTERN_OPERATIONS + "(\\()?" + PATTERN_NUMBERS + "(\\))?"+"$";
     public static final String PRINT_DOUBLE_FORMAT = "%5.2f\n";
     public static final String RIGHT_BRACKET = ")";
     public static final String LEFT_BRACKET = "(";
@@ -12,4 +12,5 @@ public class Constants {
     public static final String INCORRECT_INPUT = "Некоректный ввод! Попробуйте еще";
     public static final String OPERATION_NOT_FOUND = "Ошибка! Операция не найдена";
     public static final String DIVISION_ZERO = "Ошибка! Деление на ноль";
+    //2+9/0
 }
